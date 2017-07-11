4acacefb05c6acc2edc548767311e22df8378c5e
PByzdra <yngwie@o2.pl>
2017-07-11 20:42:21 +0200

    Exercise2-calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+/**
+ * Created by Recording on 2017-07-11.
+ */
+public class Calculator {
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+    public int sub(int a, int b) {
+
+        return a - b;
+    }
+}
