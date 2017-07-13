29229e9ab7428b933c67edd47b1ae2e2f91f18ad
PByzdra <yngwie@o2.pl>
2017-07-13 19:42:54 +0200

    Test2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,27 +1,6 @@
 package com.kodilla.testing;
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 
 public class TestingMain {
-    public static void main(String[] args) {
-//        SimpleUser simpleUser = new SimpleUser("theForumUser");
-//
-//        String result = simpleUser.getUsername();
-//
-//       if(result.equals("theForumUser")) {
-//           System.out.println("test OK");
-//       } else {
-//           System.out.println("Error!");
-//       }
-
-        Calculator calculator = new Calculator();
-        int adding = calculator.add(2,2);
-        int substracting = calculator.sub(2,2);
-       if((adding == 4) && (substracting == 0)) {
-           System.out.println("Calculator test ok");
-       } else {
-           System.out.println("Calculator test failed");
-       }
+    public static void main(String[] args){
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -10,6 +10,4 @@ public class SimpleUser {
     public String getUsername() {
         return username;
     }
-
 }
-
