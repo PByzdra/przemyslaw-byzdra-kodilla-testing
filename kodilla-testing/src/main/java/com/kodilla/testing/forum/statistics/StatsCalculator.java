998c92d3a9ebe7eaf81a4685da104d62f33a82b0
PByzdra <yngwie@o2.pl>
2017-07-22 15:06:46 +0200

    Exercise 6.5 Mockito Forum Stats #3

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatsCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatsCalculator.java
@@ -1,12 +1,37 @@
 package com.kodilla.testing.forum.statistics;
 
 public class StatsCalculator {
+
+    Statistics statistics;
+
+    public StatsCalculator(Statistics statistics) {
+        this.statistics = statistics;
+    }
+    public StatsCalculator() {
+
+    }
+
     private double numberOfUsers;
     private double numberOfPosts;
     private double numberOfComments;
     private double avgPostsPerUser;
     private double avgCommentsPerUser;
     private double avgCommentsPerPost;
+
+
+
+    public StatsCalculator(double numberOfUsers, double numberOfPosts, double numberOfComments ,
+                           double avgPostsPerUser, double avgCommentsPerUser,
+                           double avgCommentsPerPost) {
+        this.numberOfUsers = numberOfUsers;
+        this.numberOfPosts = numberOfPosts;
+        this.numberOfComments = numberOfComments;
+        this.avgPostsPerUser = avgPostsPerUser;
+        this.avgCommentsPerUser = avgCommentsPerUser;
+        this.avgCommentsPerPost = avgCommentsPerPost;
+
+    }
+
 
     public double getNumberOfUsers() {
         return numberOfUsers;
@@ -21,41 +46,22 @@ public class StatsCalculator {
     }
 
     public double getAvgPostsPerUser() {
-        return avgPostsPerUser;
+        return numberOfPosts / numberOfUsers;
     }
 
     public double getAvgCommentsPerUser() {
-        return avgCommentsPerUser;
+        return  numberOfComments / numberOfUsers;
     }
 
     public double getAvgCommentsPerPost() {
-        return avgCommentsPerPost;
-    }
-
-    public StatsCalculator(double numberOfUsers, double numberOfPosts, double numberOfComments,
-                           double avgPostsPerUser, double avgCommentsPerUser,
-                           double avgCommentsPerPost) {
-        this.numberOfUsers = numberOfUsers;
-        this.numberOfPosts = numberOfPosts;
-        this.numberOfComments = numberOfComments;
-        this.avgPostsPerUser = avgPostsPerUser;
-        this.avgCommentsPerUser = avgCommentsPerUser;
-        this.avgCommentsPerPost = avgCommentsPerPost;
-
-    }
-
-    Statistics statistics;
-
-    public StatsCalculator() {
+        return numberOfComments / numberOfPosts;
     }
 
     public void calculateAdvStatistics() {
         this.numberOfUsers = statistics.usersNames().size();
         this.numberOfPosts = statistics.postsCount();
         this.numberOfComments = statistics.commentsCount();
-        this.avgPostsPerUser = numberOfPosts / numberOfUsers;
-        this.avgCommentsPerUser = numberOfComments / numberOfUsers;
-        this.avgCommentsPerPost = numberOfComments / numberOfPosts;
+       // return statistics;
     }
 }
 
