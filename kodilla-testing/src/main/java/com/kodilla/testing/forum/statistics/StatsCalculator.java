78961fcdb02bc42677b1c0222959f2425056db32
PByzdra <yngwie@o2.pl>
2017-07-22 15:32:51 +0200

    Exercise 6.5 Mockito Forum Stats #3

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatsCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatsCalculator.java
@@ -2,36 +2,14 @@ package com.kodilla.testing.forum.statistics;
 
 public class StatsCalculator {
 
-    Statistics statistics;
+    private Statistics statistics;
+    private double numberOfUsers;
+    private double numberOfPosts;
+    private double numberOfComments;
 
     public StatsCalculator(Statistics statistics) {
         this.statistics = statistics;
     }
-    public StatsCalculator() {
-
-    }
-
-    private double numberOfUsers;
-    private double numberOfPosts;
-    private double numberOfComments;
-    private double avgPostsPerUser;
-    private double avgCommentsPerUser;
-    private double avgCommentsPerPost;
-
-
-
-    public StatsCalculator(double numberOfUsers, double numberOfPosts, double numberOfComments ,
-                           double avgPostsPerUser, double avgCommentsPerUser,
-                           double avgCommentsPerPost) {
-        this.numberOfUsers = numberOfUsers;
-        this.numberOfPosts = numberOfPosts;
-        this.numberOfComments = numberOfComments;
-        this.avgPostsPerUser = avgPostsPerUser;
-        this.avgCommentsPerUser = avgCommentsPerUser;
-        this.avgCommentsPerPost = avgCommentsPerPost;
-
-    }
-
 
     public double getNumberOfUsers() {
         return numberOfUsers;
@@ -50,7 +28,7 @@ public class StatsCalculator {
     }
 
     public double getAvgCommentsPerUser() {
-        return  numberOfComments / numberOfUsers;
+        return numberOfComments / numberOfUsers;
     }
 
     public double getAvgCommentsPerPost() {
@@ -61,7 +39,6 @@ public class StatsCalculator {
         this.numberOfUsers = statistics.usersNames().size();
         this.numberOfPosts = statistics.postsCount();
         this.numberOfComments = statistics.commentsCount();
-       // return statistics;
     }
 }
 
